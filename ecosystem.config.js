8f8c4b2133be0b7f0a73eacce4ff0b5120f39479
Ruben Requena <rubnrequena@gmail.com>
2021-04-30 20:14:24 -0400

    fixes

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [{
-    script: 'index.js',
-    name: "srq.twitter",
-    env: {
-      PORT: 3003
-    }
-  }],
-};
---
+++ src/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [{
+    script: './build/index.js',
+    name: "srq.twitter",
+    env: {
+      PORT: 3003,
+	  CHROME_PATH:"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+	  CHROME_HEADLESS="true"
+    }
+  }],
+};
